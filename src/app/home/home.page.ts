542e3dfa2a13bc4c9c46cbd91186934ef7545c80
Ajoymaity <ajoy.maity@tarento.com>
2023-06-13 13:25:30 +0530

    Issue #L-App feat: IMplemented QR scanner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private barcodeScanner: BarcodeScanner
+  ) {}
+  
+  openQRScanner() {
+    this.barcodeScanner.scan().then(async (barcodeData) => {
+      console.log('Barcode data', barcodeData);
+      const requestParam: NavigationExtras = {
+        state: {url: barcodeData.text.split('?')[1]}
+      }
+      this.router.navigate(['./chapter-details-option'], requestParam);
+ 
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
 
 }
