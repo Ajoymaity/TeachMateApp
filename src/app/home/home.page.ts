e043ddf336f645befb7e87eaff8aa3f3063e1921
Ajoymaity <ajoy.maity@tarento.com>
2023-06-13 16:17:42 +0530

    Issue #Learn feat: URL

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,15 +13,26 @@ export class HomePage {
     private router: Router,
     private barcodeScanner: BarcodeScanner
   ) {}
-  
+
+  URLToObject(url: any) {
+    var request: any = {};
+    var pairs = url.split('?')[1].split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        if(!pairs[i])
+            continue;
+        
+        var pair = pairs[i].split('=');
+        request[pair[0]] = pair[1];
+     }
+     return request;
+}
   openQRScanner() {
     this.barcodeScanner.scan().then(async (barcodeData) => {
       console.log('Barcode data', barcodeData);
       const requestParam: NavigationExtras = {
-        state: {url: barcodeData.text.split('?')[1]}
+        state: this.URLToObject(barcodeData.text)
       }
       this.router.navigate(['./chapter-details-option'], requestParam);
- 
      }).catch(err => {
          console.log('Error', err);
      });
